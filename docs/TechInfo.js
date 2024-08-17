9554803120129258f4c6885dadce144bb3036712
valentin <vb.brebionpro@gmail.com>
2024-08-17 16:26:51 +0200

    [techInfo: add] build first concept of new rubric TechInfo

---
+++ docs/TechInfo.js
@@ -0,0 +1,14 @@
+const informations = [
+    { id: 1, titre: "Sym' Français", contenu: "Saviez-vous que le framework PHP Symfony est d'origine française ? Il a été créé par Fabien Potencier et est maintenu par SensioLabs." },
+    { id: 2, titre: "Le premier virus informatique", contenu: "Le premier virus informatique connu est le 'Creeper', créé en 1971 par Bob Thomas. Il était conçu pour se répandre sur les systèmes ARPANET, le précurseur de l'Internet." },
+    { id: 3, titre: "Le premier IDE", contenu: "L'un des premiers environnements de développement intégré (IDE) était le 'Turbo Pascal', développé par Borland dans les années 1980, offrant à la fois un éditeur de code et un compilateur dans une seule interface." },
+    { id: 4, titre: "Le langage Fortran", contenu: "Fortran, abréviation de 'Formula Translation', est l'un des premiers langages de programmation de haut niveau. Développé dans les années 1950, il est encore utilisé aujourd'hui pour des applications scientifiques et techniques." },
+    { id: 5, titre: "La loi de Moore", contenu: "Formulée par Gordon Moore en 1965, cette loi stipule que le nombre de transistors sur une puce double environ tous les deux ans, augmentant ainsi la puissance de calcul des ordinateurs." },
+    { id: 6, titre: "Le mouvement open source", contenu: "Le mouvement open source a été popularisé par Richard Stallman avec la Free Software Foundation en 1985 et par Linus Torvalds avec le noyau Linux en 1991, promouvant le partage libre du code source." },
+    { id: 7, titre: "Ada Lovelace", contenu: "Ada Lovelace, une mathématicienne du XIXe siècle, est souvent considérée comme la première programmeuse pour ses algorithmes destinés à la machine analytique de Charles Babbage." },
+    { id: 8, titre: "La première interface graphique", contenu: "L'interface graphique utilisateur (GUI) a été popularisée par le Xerox Alto dans les années 1970 et rendue célèbre par le Macintosh d'Apple en 1984." },
+    { id: 9, titre: "Le langage Python", contenu: "Python, créé par Guido van Rossum et publié en 1991, est renommé pour sa simplicité et sa lisibilité, et est largement utilisé dans des domaines allant du développement web à l'analyse de données." },
+    { id: 10, titre: "Les langages fonctionnels", contenu: "Les langages de programmation fonctionnels, comme Haskell et Lisp, traitent les fonctions comme des valeurs et évitent les effets secondaires, ce qui peut conduire à des programmes plus prévisibles et modulaires." }
+];
+
+export default informations;
