e60d84bdc5db6f1039e8749119d264a3c9f67318
valentin <vb.brebionpro@gmail.com>
2025-05-28 19:54:20 +0200

    add theme light

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -34,8 +34,58 @@ export const shared = defineConfig({
             }
         },
         search: {
-            provider: 'local'
-        },
+          provider: 'local',
+          options: {
+              locales: {
+                  root: { // pour le français
+                      translations: {
+                          button: {
+                              buttonText: 'Rechercher',
+                              buttonAriaLabel: 'Rechercher'
+                          },
+                          modal: {
+                              displayDetails: 'Afficher la liste détaillée',
+                              resetButtonTitle: 'Réinitialiser la recherche',
+                              backButtonTitle: 'Fermer la recherche',
+                              noResultsText: 'Aucun résultat',
+                              footer: {
+                                  selectText: 'Sélectionner',
+                                  selectKeyAriaLabel: 'Entrer',
+                                  navigateText: 'Navigation',
+                                  navigateUpKeyAriaLabel: 'Flèche du haut',
+                                  navigateDownKeyAriaLabel: 'Flèche du bas',
+                                  closeText: 'Fermer',
+                                  closeKeyAriaLabel: 'esc'
+                              }
+                          }
+                      }
+                  },
+                  en: { // pour l'anglais
+                      translations: {
+                          button: {
+                              buttonText: 'Search',
+                              buttonAriaLabel: 'Search'
+                          },
+                          modal: {
+                              displayDetails: 'Show detailed list',
+                              resetButtonTitle: 'Reset search',
+                              backButtonTitle: 'Close search',
+                              noResultsText: 'No results',
+                              footer: {
+                                  selectText: 'Select',
+                                  selectKeyAriaLabel: 'Enter',
+                                  navigateText: 'Navigation',
+                                  navigateUpKeyAriaLabel: 'Arrow up',
+                                  navigateDownKeyAriaLabel: 'Arrow down',
+                                  closeText: 'Close',
+                                  closeKeyAriaLabel: 'esc'
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      },
         darkModeSwitchLabel: 'Appearance',
         socialLinks: [
             { icon: 'github', link: 'https://github.com/ValentinBrebion' }
